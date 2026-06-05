5e62dcd29a4326964799a4b86e1200b393b6b7be
byfonz <fonz.contact@gmail.com>
2026-06-05 13:04:35 -0700

    ci(hyprdynamicmonitors): stowed configuration files in repo

--- config/hypr/workspaces.lua
+++ config/hypr/workspaces.lua
@@ -1,26 +1,21 @@
 hl.workspace_rule({
   workspace = "1",
-  monitor = "DP-1",
 })
 
 hl.workspace_rule({
   workspace = "2",
-  monitor = "HDMI-A-1",
 })
 
 hl.workspace_rule({
   workspace = "3",
-  monitor = "HDMI-A-1",
 })
 
 hl.workspace_rule({
   workspace = "4",
-  monitor = "HDMI-A-1",
 })
 
 hl.workspace_rule({
   workspace = "5",
-  monitor = "HDMI-A-1",
 })
 
 ---------------------
@@ -31,16 +26,7 @@ hl.window_rule({
   name = "system",
   match = {
     class = "com.mitchellh.ghostty",
-    title = "btop",
-  },
-  workspace = "1",
-})
-
-hl.window_rule({
-  name = "system",
-  match = {
-    class = "com.mitchellh.ghostty",
-    title = "ponchoio@ponchoio: ~",
+    title = "tmux",
   },
   workspace = "1",
 })
@@ -51,26 +37,26 @@ hl.window_rule({
   match = {
     class = "com.mitchellh.ghostty",
     title = "nvim",
+    workspace = "2",
   },
-  workspace = "2",
 })
 
 hl.window_rule({
-  name = "git",
+  name = "terminal",
   match = {
     class = "com.mitchellh.ghostty",
-    title = "git",
+    title = "tmux",
+    workspace = "2",
   },
-  workspace = "2",
 })
 
 hl.window_rule({
   name = "render",
   match = {
     class = "brave-browser",
-    title = "render",
+    title = "localhost",
+    workspace = "2",
   },
-  workspace = "2",
 })
 
 --- Workspace 3: browser
@@ -78,16 +64,15 @@ hl.window_rule({
   name = "browser",
   match = {
     class = "brave-browser",
+    fullscreen = true,
+    workspace = "3",
   },
-  fullscreen = true,
-  opacity = "1.0 override 0.85",
-  workspace = "3",
 })
 
 hl.window_rule({
   name = "notes",
   match = {
     class = "obsidian",
+    workspace = "4",
   },
-  workspace = "4",
 })
