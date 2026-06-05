55fca07684d25f26b5a25f381ae832589ab05cc7
byfonz <fonz.contact@gmail.com>
2026-06-04 19:31:05 -0700

    refactor(hypr): refactored hyprland startup script, refactored notes and monitor workspaces issues

--- config/hypr/autostart.lua
+++ config/hypr/autostart.lua
@@ -1,7 +1,9 @@
 ---------------------
 ---- APPS ----
 ---------------------
-local codeWorskpace = "~/Workspace/workspace-os/scripts/code_workspace"
+local systemWorkspace = "~/Workspace/workspace-os/scripts/system_monitor_workspace"
+local codeWorkspace = "~/Workspace/workspace-os/scripts/code_workspace"
+local notesWorkspace = "~/Workspace/workspace-os/scripts/notes_workspace"
 
 -------------------
 ---- AUTOSTART ----
@@ -9,5 +11,7 @@ local codeWorskpace = "~/Workspace/workspace-os/scripts/code_workspace"
 hl.on("hyprland.start", function()
   hl.exec_cmd("hyprpaper")
   hl.exec_cmd("waybar")
-  hl.exec_cmd(codeWorskpace)
+  hl.exec_cmd(systemWorkspace)
+  hl.exec_cmd(codeWorkspace)
+  hl.exec_cmd(notesWorkspace)
 end)
