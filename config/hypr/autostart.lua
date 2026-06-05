e8a3821ef7b358dbce0ca899f74802946a692a55
byfonz <fonz.contact@gmail.com>
2026-06-04 17:23:14 -0700

    refactor(hypr): refactored hyprland autostart config

--- config/hypr/autostart.lua
+++ config/hypr/autostart.lua
@@ -1,13 +1,7 @@
 ---------------------
 ---- APPS ----
 ---------------------
-local terminal = "ghostty"
-local btop = "ghostty -e btop"
-local fastfetch = "ghostty -e ~/Workspace/workspace-os/scripts/startup/fastfetch"
-local nvim = "ghostty --title nvim -e nvim ~/Workspace/"
-local git = "ghostty --title git"
-local obsidian = "obsidian --new-window"
-local localhost = "brave --new-window http://localhost:3000 "
+local codeWorskpace = "~/Workspace/workspace-os/scripts/code_workspace"
 
 -------------------
 ---- AUTOSTART ----
@@ -15,11 +9,5 @@ local localhost = "brave --new-window http://localhost:3000 "
 hl.on("hyprland.start", function()
   hl.exec_cmd("hyprpaper")
   hl.exec_cmd("waybar")
-  hl.exec_cmd(btop)
-  hl.exec_cmd(fastfetch)
-  hl.exec_cmd(nvim)
-  hl.exec_cmd(git)
-  hl.exec_cmd("brave --password-store=basic")
-  hl.exec_cmd(localhost)
-  hl.exec_cmd(obsidian)
+  hl.exec_cmd(codeWorskpace)
 end)
